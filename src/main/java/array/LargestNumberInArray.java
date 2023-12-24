9b418dee3fc18c747cfca38af9d3eaccc0e2d52d
vrushali <vrushali.r@spenmo.com>
2023-12-24 18:23:47 +0530

    largest element in the array

--- src/main/java/array/LargestNumberInArray.java
+++ src/main/java/array/LargestNumberInArray.java
@@ -35,10 +35,30 @@ public class LargestNumberInArray {
 
         int arr2[] =  {8,10,5,7,9};
         System.out.println("The Largest element in the array is: " + sort(arr2));
+
+
+        System.out.println("The Largest element in the array is: "+findLargestElement(arr1));
+        System.out.println("The Largest element in the array is: "+findLargestElement(arr2));
     }
 
+    //bruitforce
+    // tc - n log n
+    //sc - o (1)
     static int sort(int arr[]) {
         Arrays.sort(arr);
         return arr[arr.length - 1];
+    }
+
+    //optimal
+    // tc - o(n)
+    //sc - o (1)
+    static int findLargestElement(int arr[]) {
+        int max= arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max= arr[i];
+            }
+        }
+        return max;
     }
 }
